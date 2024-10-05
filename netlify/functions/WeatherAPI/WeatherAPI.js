72772cd499d000c81b5b4dd4bc04a35efdc1e5e1
basir <basir.folaad24@gmail.com>
2024-10-05 16:24:13 -0300

    Brought changes to function

--- netlify/functions/WeatherAPI/WeatherAPI.js
+++ netlify/functions/WeatherAPI/WeatherAPI.js
@@ -1,17 +1,20 @@
-
 const handler = async (event) => {
   try {
     const apiKEY = process.env.WeatherAPI;
     const response = await fetch(`https://stirring-khapse-dd80ba.netlify.app/.netlify/functions/WeatherAPI?api_key=${apiKEY}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const data = await response.json();
     return {
       statusCode: 200,
       body: JSON.stringify(data),
-      
-    }
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
