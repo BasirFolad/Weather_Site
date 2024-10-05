5b48adba0e0a1e5180b272badb4dc6d767260f79
basir <basir.folaad24@gmail.com>
2024-10-05 16:13:03 -0300

    Add changes on serverless function

--- netlify/functions/WeatherAPI/WeatherAPI.js
+++ netlify/functions/WeatherAPI/WeatherAPI.js
@@ -1,13 +1,13 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const apiKEY = process.env.WeatherAPI;
+    const response = await fetch(`https://stirring-khapse-dd80ba.netlify.app/.netlify/functions/WeatherAPI?api_key=${apiKEY}`);
+    const data = await response.json();
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify(data),
+      
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
